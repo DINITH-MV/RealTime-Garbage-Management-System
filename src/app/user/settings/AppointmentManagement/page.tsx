1ecdf378d74e545b6e2ff11639acd2ff46fb6797
dinusanka gayashan <dinuska.g@gmail.com>
2024-10-23 21:11:12 +0530

    userid
    userid

--- src/app/user/settings/AppointmentManagement/page.tsx
+++ src/app/user/settings/AppointmentManagement/page.tsx
@@ -1,4 +1,5 @@
 import Analytics from "@/components/Dashboard/Analytics";
+import { auth } from "@clerk/nextjs/server";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { checkRole } from "@/utils/roles";
@@ -12,10 +13,12 @@ export const metadata: Metadata = {
 export default async function Settings() {
   const isAdmin = checkRole("admin"); // Perform role check server-side
 
+  const { userId } = await auth();
+  
   return (
     <>
       <DefaultLayout isAdmin={isAdmin}>
-        <ManagementAppointment />
+        <ManagementAppointment userId={userId}/>
       </DefaultLayout>
     </>
   );
